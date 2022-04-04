7d74d7878374b292cdff72284d1c0dd9760240f0
Elton Maiyo <maiyo.elton@gmail.com>
2022-04-04 20:06:43 +0300

    Utilizes x clearly to highlight difference in by-value and by-name

--- src/main/scala/lectures/part1basics/CBNvsCBV.scala
+++ src/main/scala/lectures/part1basics/CBNvsCBV.scala
@@ -3,13 +3,13 @@ package lectures.part1basics
 object CBNvsCBV extends App {
 
   def calledByValue(x: Long): Unit = {
-    println("by value: " + 1257387745764245L)
-    println("by value: " + 1257387745764245L)
+    println("by value: " + x)
+    println("by value: " + x)
   }
 
   def calledByName(x: => Long): Unit = {
-    println("by name: " + System.nanoTime())
-    println("by name: " + System.nanoTime())
+    println("by name: " + x)
+    println("by name: " + x)
   }
 
   calledByValue(1257387745764245L)
